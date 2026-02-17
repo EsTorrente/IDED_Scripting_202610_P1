76125169d5de99a28f317b19f8b523a475e4663a
EsTorrente <maria.torrente@upb.edu.co>
2026-02-16 21:47:07 -0500

    Arreglado todo lo que estaba malo XD

--- TestMethods.cs
+++ TestMethods.cs
@@ -4,55 +4,38 @@
     {
         public static void SeparateElements(Queue<int> input, out Stack<int> included, out Stack<int> excluded)
         {
-            included = null;
-            excluded = null;
+            included = new Stack<int>();
+            excluded = new Stack<int>();
 
-            Stack<int> tempIncluded = new Stack<int>(); //para invertirlas después manualmente :P
-            Stack<int> tempExcluded = new Stack<int>();
-
-            int count = input.Count;
-
-            for (int i = 0; i < count; i++)
+            foreach (int number in input)
             {
-                int number = input.Dequeue(); //me devuelve el último elemento de la lista
+                int absValue = Math.Abs(number);
+                int root = (int)Math.Sqrt(absValue);
 
-                int absValue = number < 0 ? -number : number; //si es negativo, invierte el signo; si es pos, lo deja igual
-
-                int root = (int)Math.Sqrt(absValue); //chequeo condición con la raíz cuadrada, entonces la saco COMO ENTERO para ver si tiene decimales
-
-                bool isPerfectSquare = root * root == absValue; //condición 1. tiene que ser cuadrado perfecto. Si el root que saqué no coincide, tenía decimales
-
-                bool belongs = false; //condición para sortearlo después
+                bool isPerfectSquare = root * root == absValue;
+                bool belongs = false;
 
                 if (isPerfectSquare)
                 {
-                    if (root % 2 != 0 && number < 0) //si al raíz es impar, el num debe ser negativo
+                    if (root % 2 != 0 && number < 0)
                         belongs = true;
 
-                    if (root % 2 == 0 && number > 0) //si la raíz es par, el num debe ser pos
+                    if (root % 2 == 0 && number > 0)
                         belongs = true;
                 }
 
                 if (belongs)
-                    tempIncluded.Push(number);
+                    included.Push(number);
                 else
-                    tempExcluded.Push(number);
+                    excluded.Push(number);
             }
-
-            //aquí los invierto en el stack final :>
-            while (tempIncluded.Count > 0)
-                included.Push(tempIncluded.Pop());
-
-            while (tempExcluded.Count > 0)
-                excluded.Push(tempExcluded.Pop());
         }
 
         public static List<int> GenerateSortedSeries(int n)
         {
             List<int> result = new List<int>();
 
-            // Generar la sucesión
-            for (int i = 1; i <= n; i++)
+            for (int i = 0; i < n; i++)
             {
                 int value = i * i;
 
@@ -62,19 +45,7 @@
                 result.Add(value);
             }
 
-            for (int i = 0; i < result.Count - 1; i++)
-            {
-                for (int j = 0; j < result.Count - 1 - i; j++)
-                {
-                    if (result[j] > result[j + 1])
-                    {
-                        int temp = result[j];
-                        result[j] = result[j + 1];
-                        result[j + 1] = temp;
-                    }
-                }
-            }
-
+            result.Sort();
             return result;
         }
 
@@ -111,17 +82,8 @@
 
         public static int FindPrime(in Stack<int> list)
         {
-            Stack<int> copy = new Stack<int>();
-
-            foreach (int value in list)
+            foreach (int number in list)
             {
-                copy.Push(value);
-            }
-
-            while (copy.Count > 0)
-            {
-                int number = copy.Pop();
-
                 if (IsPrime(number))
                     return number;
             }
@@ -145,53 +107,27 @@
 
         public static Stack<int> RemoveFirstPrime(in Stack<int> stack)
         {
-            Stack<int> copy = new Stack<int>();
             Stack<int> temp = new Stack<int>();
-
-            foreach (int value in stack)
-            {
-                copy.Push(value);
-            }
-
-            while (copy.Count > 0)
-            {
-                temp.Push(copy.Pop());
-            }
-
-            Stack<int> result = new Stack<int>();
             bool removed = false;
 
-            while (temp.Count > 0)
+            foreach (int number in stack)
             {
-                int number = temp.Pop();
-
                 if (!removed && IsPrime(number))
                 {
-                    removed = true; 
+                    removed = true;
                     continue;
                 }
 
-                result.Push(number);
+                temp.Push(number);
             }
 
-            return result;
+            return new Stack<int>(temp);
         }
 
         public static Queue<int> QueueFromStack(Stack<int> stack)
         {
-            Stack<int> copy = new Stack<int>();
-            Stack<int> temp = new Stack<int>();
+            Stack<int> temp = new Stack<int>(stack);
             Queue<int> result = new Queue<int>();
-
-            foreach (int value in stack)
-            {
-                copy.Push(value);
-            }
-
-            while (copy.Count > 0)
-            {
-                temp.Push(copy.Pop());
-            }
 
             while (temp.Count > 0)
             {
